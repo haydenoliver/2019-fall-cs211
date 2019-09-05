03966dc91c483116a08dfa58dd219205cc11c687
Hayden Oliver <haydenboliver@gmail.com>
2019-09-05 01:54:49 -0700

    I have commented out the border section and started a menu section. The menu is not yet completed, only the box for it is. I plan to add options into the menu. I also made the "Text Editor" title have a green background. I plan to use the colors in the menu as well

--- projects/TextEditor/src/TextEdit/TextEdit/Main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/Main.cpp
@@ -1,18 +1,20 @@
 #define PDC_DLL_BUILD 1
 #include "curses.h"
 #include <string>
+#include <iostream>
 
 using namespace std;
 
-int main(void)
+
+int main( void)
 {
-	WINDOW* main_window = nullptr;
+	/*WINDOW* main_window = nullptr;
 	int num_rows = 0;
 	int num_cols = 0;
 
 
 	//inititalize screen, begin curses mode
-	main_window = initscr();
+	initscr();
 
 	//take up most of the screen
 	getmaxyx(main_window, num_rows, num_cols);
@@ -30,27 +32,88 @@ int main(void)
 	for (int i = 0; i < num_cols; i++)
 	{
 		//top row
-		mvaddch(0, i, ACS_CKBOARD);
+		mvaddch(0, i, ACS_LLCORNER);
 		//bottom row
-		mvaddch(num_rows - 1, i, ACS_CKBOARD);
+		mvaddch(num_rows - 1, i, ACS_LLCORNER);
 	}
 	for (int i = 0; i < num_rows; i++)
 	{
 		//left column
-		mvaddch(i, 0, ACS_CKBOARD);
+		mvaddch(i, 0, ACS_LLCORNER);
 		//right column
-		mvaddch(i, num_cols - 1, ACS_CKBOARD);
+		mvaddch(i, num_cols - 1, ACS_LLCORNER);
 	}
 	//tells curses to draw
 	refresh(); 
 	char result = getch();
 	//revert back to normal console mode
 
-	
-	nodelay(main_window, TRUE);
-	keypad(main_window, TRUE);
-	mvaddstr(0, 0, "Press any key to continue");
 
+	endwin(); */
+
+
+	/*initscr();
+
+	int height, width, start_x, start_y;
+
+	height = 10;
+	width = 20;
+	start_y = start_x = 10;
+
+	WINDOW* win = newwin(height, width, start_x, start_y);
+	refresh();
+	//char c = 'g';
+
+ 	box(win, 0, 0);
+	mvwprintw(win, 1, 1, "text editor");
+	wrefresh(win);
+
+
+	endwin();*/
+
+	initscr();
+	noecho();
+	cbreak();
+
+	//gives text a colored background
+	start_color();
+	init_pair(1, COLOR_BLACK, COLOR_GREEN);
+	attron(COLOR_PAIR(1));
+	printw("Text Editor");
+	refresh();
+	
+	//makes a menu box
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax);
+
+	WINDOW* menuwin = newwin(10, xMax - 100, yMax - 10, .1);
+	box(menuwin, 0, 0);
+	refresh();
+	wrefresh(menuwin);
+
+
+	//adds choices to the menu box (not finished yet)
+	string menubar[5] = { "font", "color", "size", "bold", "underline" };
+	int highlight = 0;
+	//int choice;
+
+	//keypad(menuwin, true);
+
+	
+
+
+	getch();
 	endwin();
 	
-}
+
+	return 0;
+	
+
+	
+	
+		}
+	
+
+
+	
+
