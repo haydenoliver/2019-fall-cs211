7c0c8804b1d6a1f7e044dbf7e41e74bf2637e73d
Hayden Oliver <haydenboliver@gmail.com>
2019-09-10 22:27:14 -0700

    I have finalized Milestone #1 with a title to the page "Text Editor" and a box for a menu. In milestone 2 I will insert document into my console.

--- projects/TextEditor/src/TextEdit/TextEdit/Main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/Main.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 int main( void)
 {
-	/*WINDOW* main_window = nullptr;
+	/*
+	//sample border
+	WINDOW* main_window = nullptr;
 	int num_rows = 0;
 	int num_cols = 0;
 
@@ -26,54 +28,15 @@ int main( void)
 	nodelay(main_window, TRUE);
 	keypad(main_window, TRUE);
 	curs_set(0);
+	*/
+	
 
-	//fun stuff happens here
-
-	for (int i = 0; i < num_cols; i++)
-	{
-		//top row
-		mvaddch(0, i, ACS_LLCORNER);
-		//bottom row
-		mvaddch(num_rows - 1, i, ACS_LLCORNER);
-	}
-	for (int i = 0; i < num_rows; i++)
-	{
-		//left column
-		mvaddch(i, 0, ACS_LLCORNER);
-		//right column
-		mvaddch(i, num_cols - 1, ACS_LLCORNER);
-	}
-	//tells curses to draw
-	refresh(); 
-	char result = getch();
-	//revert back to normal console mode
-
-
-	endwin(); */
-
-
-	/*initscr();
-
-	int height, width, start_x, start_y;
-
-	height = 10;
-	width = 20;
-	start_y = start_x = 10;
-
-	WINDOW* win = newwin(height, width, start_x, start_y);
-	refresh();
-	//char c = 'g';
-
- 	box(win, 0, 0);
-	mvwprintw(win, 1, 1, "text editor");
-	wrefresh(win);
-
-
-	endwin();*/
-
+	//initializes window
 	initscr();
 	noecho();
-	cbreak();
+	//user control in menu... 
+	//keypad(menuwin, true);
+	
 
 	//gives text a colored background
 	start_color();
@@ -82,22 +45,25 @@ int main( void)
 	printw("Text Editor");
 	refresh();
 	
+
+
+
 	//makes a menu box
 	int yMax, xMax;
 	getmaxyx(stdscr, yMax, xMax);
+	WINDOW* menuwin = newwin(10, xMax - 100, yMax - 10, 1);
+	box(menuwin, ACS_DIAMOND, ACS_VLINE);
+	
+	
 
-	WINDOW* menuwin = newwin(10, xMax - 100, yMax - 10, .1);
-	box(menuwin, 0, 0);
 	refresh();
 	wrefresh(menuwin);
 
 
 	//adds choices to the menu box (not finished yet)
 	string menubar[5] = { "font", "color", "size", "bold", "underline" };
-	int highlight = 0;
-	//int choice;
-
-	//keypad(menuwin, true);
+	
+	
 
 	
 
